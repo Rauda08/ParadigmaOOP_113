8efa0dfc127cba801d5ae869473fe37bfac6b388
rauda08 <raudajnnh08@gmail.com>
2023-05-22 11:38:54 +0700

    menampilkan display pelajar

--- ParadigmaOOP_113/ParadigmaOOP_113.cpp
+++ ParadigmaOOP_113/ParadigmaOOP_113.cpp
@@ -23,4 +23,11 @@ public:
 class pelajar: public orang {
     public:
     string sekolah;
+
+    pelajar(string pNama, string pSekolah) :
+        orang(pNama),
+        sekolah(pSekolah) {
+        cout << "pelajar dihapus\n" << endl;
+    }
+
 };
