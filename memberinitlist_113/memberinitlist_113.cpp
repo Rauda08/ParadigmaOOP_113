797829c6e65dae65232e7a48a1920ec6093f965f
rauda08 <raudajnnh08@gmail.com>
2023-05-22 21:39:40 +0700

    membuat member initialization

--- memberinitlist_113/memberinitlist_113.cpp
+++ memberinitlist_113/memberinitlist_113.cpp
@@ -2,7 +2,15 @@
 #include <string>
 using namespace std;
 
-class mahasiswa {};
+class mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Member initialization List}
+};
 
 int main()
 {
