44b291d5377bf18015e53a8cbdc99ebbf114eb97
rauda08 <raudajnnh08@gmail.com>
2023-05-22 21:51:33 +0700

    menghapus variabel ibu dan anak

--- MainIA_113/Header.h
+++ MainIA_113/Header.h
@@ -12,3 +12,16 @@ int main() {
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Dewi");
 
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varibu2->tambahAnak(varAnak3);
+    varibu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varibu2->cetakAnak();
+
+    delete varIbu;
+    delete varibu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
