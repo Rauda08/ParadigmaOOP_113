ef3718357aced0585b154d53dead763140806d6b
rauda08 <raudajnnh08@gmail.com>
2023-05-22 21:47:38 +0700

    mengakhiri

--- MainIA_113/Header.h
+++ MainIA_113/Header.h
@@ -7,6 +7,11 @@ public:
     anak(string pNama) :nama(pNama) {
         cout << "Anak \"" << nama << "\" ada\n";
     }
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
 
 
 
